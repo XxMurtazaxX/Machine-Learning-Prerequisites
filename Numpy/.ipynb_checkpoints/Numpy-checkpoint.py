59a56248e8491f63b96d6fb5094da7a86150fda4
Murtaza <murtaza.jerryhere@gmail.com>
2024-08-28 22:02:14 +0500

    Jupyter Notebooks: Numpy and Pandas

---
+++ Numpy/.ipynb_checkpoints/Numpy-checkpoint.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+# 2D matrix
+a = np.array([[1, 2, 3], [2, 4, 6], [6, 12, 24]])
+
+# 1D matrix
+b = np.array([4, 64, 49, 8])
+x = np.array([6, 4, 3, 2])
+
+# print(b + x)
+# print(np.add(b, x))
+print(b-x)
+print(np.subtract(b, x))
+# print(np.sqrt(b))
+# print(np.power(a, 6))
+# print(b*x)
+# print(np.multiply(b, x))
+
+
+
+# print(np.transpose(a))
+
+# print(np.dot(a, b))
+# print(np.shape(x))
+# print(np.x)
